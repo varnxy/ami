4b7eac26bcb9b3bdc00fa4a899bab78973e12edd
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2019-03-02 01:30:02 +0700

    Add reconnect and refactor

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 const Socket = require('net').Socket
     , EventEmitter = require('events').EventEmitter
     , util = require('util')
-    , defaults = require('./default')
-    , NEW_LINE = '\r\n'
-    , END_LINE = '\r\n\r\n'
-    , COLON_SEP = ': '
+    , C = require('./constants')
+    , createMessage = require('./create-message')
+    , MessageParser = require('./message-parser')
 
 function AMI(opt) {
   opt = opt || {}
 
-  this._config = Object.assign(defaults, opt)
+  this._config = Object.assign(C.DEFAULT, opt)
 
   this._connection = null
   this._requestMap = {}
   this._connected = false
-  this._msgBuffer = ''
   this._requestQueues = []
   this._requestOnWork = false
+  this._reconnectInterval = null
+  this._reconnectTime = 0
+  this._messageParser = new MessageParser(this._asteriskResponse.bind(this))
   // This just for unique ActionID
   this._requestId = +new Date()
 }
 
 AMI.prototype.start = function() {
+  this._connection = new Socket()
+  this._connection.connect(this._config.port, this._config.host)
+  this._connection.once('connect', this._authenticate.bind(this))
+  this._initialSocket()
+}
+
+AMI.prototype._initialSocket = function() {
   let eventMap = {
     'connect': this._onConnect.bind(this),
     'close': this._onClose.bind(this),
@@ -29,13 +37,10 @@ AMI.prototype.start = function() {
     'end': this._onEnd.bind(this),
     'error': this._onError.bind(this)
   }
-  this._connection = new Socket()
 
-  this._connection.connect(this._config.port, this._config.host)
   this._connection.setKeepAlive(true)
   this._connection.setNoDelay(true)
-  this._connection.setEncoding('utf-8')
-  this._connection.once('connect', this._authenticate.bind(this))
+  this._connection.setEncoding(C.ENCODING)
 
   for (let evt in eventMap) {
     this._connection.on(evt, eventMap[evt])
@@ -47,7 +52,7 @@ AMI.prototype.action = function(name, params) {
   params.Action = name
   params.ActionID = params.ActionID || this._requestId++
 
-  let message = this._createMessage(params)
+  let message = createMessage(params)
     , promise = new Promise((resolve, reject) => {
     this._requestMap[params.ActionID] = {
       resolve: resolve,
@@ -73,69 +78,45 @@ AMI.prototype._requestWorker = function() {
   }
 }
 
-AMI.prototype._createMessage = function(obj) {
-  let msg = []
-  for (let k in obj) {
-    msg.push([k, COLON_SEP, obj[k]].join(''))
-  }
-
-  msg.push(NEW_LINE)
-
-  return msg.join(NEW_LINE)
-}
-
 AMI.prototype._authenticate = function() {
-  this._connected = true
-
-  this.action('Login', {
+  this.action(C.ACTION_LOGIN, {
     Username: this._config.username,
     Secret: this._config.secret,
-    Events: this._config.events ? 'on' : 'off'
+    Events: this._config.events ? C.RESPONSE_EVENT_ON : C.RESPONSE_EVENT_OFF
   }).then(() => {
-    this.emit('authenticated')
+    this.emit(!this._connected ? C.SIGNAL.AUTHENTICATED : C.SIGNAL.RECONNECTED)
+    this._connected = true
   }).catch((err) => {
-    this.emit('error', new Error(err.toString()))
+    this.emit(C.SIGNAL.ERROR, new Error(err.toString()))
   })
 }
 
-AMI.prototype._parseMessage = function(message) {
-  let result, endLineIndex, msgLines
-    , msgLine, msgLineSplit
-
-  this._msgBuffer += message
-
-  while ((endLineIndex = this._msgBuffer.indexOf(END_LINE)) != -1) {
-    msgLines = this._msgBuffer.substr(0, endLineIndex+2).split(NEW_LINE)
-    this._msgBuffer = this._msgBuffer.substr(endLineIndex+4)
-    result = {}
-
-    for (let i=0;i<msgLines.length;i++) {
-      msgLine = msgLines[i]
-
-      if (msgLine.indexOf(COLON_SEP) != -1) {
-        msgLineSplit = msgLine.split(COLON_SEP)
-        result[msgLineSplit[0]] = msgLineSplit[1]
-      }
-    }
-
-    if (result.Event || result.Response) {
-      this._asteriskResponse(result)
-    }
-  }
-}
-
 AMI.prototype._onConnect = function() {
-  this.emit('connected')
+  this.emit(C.SIGNAL.CONNECTED)
 }
 
 AMI.prototype._onClose = function(err) {
-  this._connected = false
-  this.emit('disconnected', err)
+  this._connection.removeAllListeners()
+
+  this.emit(C.SIGNAL.DISCONNECTED, err)
   this._connection.destroy()
 
-  if (this._config.keepAlive) {
-
+  if (this._config.keepConnected) {
+    this._reconnectTime = Math.min(this._reconnectTime+1, MAX_RECONNECT_TIME)
+    setTimeout(() => {
+      this._tryReconnect()
+    }, RECONNECT_BACKOFF * (++this._reconnectTime))
   }
+}
+
+AMI.prototype._tryReconnect = function() {
+  this._connection.connect(this._config.port, this._config.host)
+  this._initialSocket()
+
+  this._connection.once('connect', () => {
+    this._reconnectTime = 0
+    this._authenticate()
+  })
 }
 
 AMI.prototype._onEnd = function() {
@@ -144,52 +125,56 @@ AMI.prototype._onEnd = function() {
 }
 
 AMI.prototype._onError = function(err) {
-  this.emit('error', err)
+  this._connection.end()
+  this.emit(C.SIGNAL.ERROR, err)
 }
 
 AMI.prototype._onData = function(buffer) {
   let data = buffer.toString()
 
-  if (data.startsWith('Asterisk Call Manager')) {
-    data.split(NEW_LINE)
+  if (data.startsWith(C.AMI_PROTO_HEADER)) {
+    data.split(C.NEW_LINE)
         .splice(1)
-        .join(NEW_LINE)
+        .join(C.NEW_LINE)
   }
 
-  this._parseMessage(data)
+  this._messageParser.addMessage(data)
 }
 
 AMI.prototype._asteriskResponse = function(message) {
   this._requestWorker()
 
   if (this._config.debug) {
-    this.emit('debug', message)
+    this.emit(C.SIGNAL.DEBUG, message)
   }
 
   if (message.Response) {
     this._resolveAction(message)
   } else {
     let evt = message.Event
+
     delete message.Event
     this.emit(evt, message)
   }
 }
 
-AMI.prototype._resolveAction = function(msg) {
-  let requestMap = this._requestMap[msg.ActionID]
+AMI.prototype._resolveAction = function(message) {
+  let requestMap = this._requestMap[message.ActionID]
 
   if (!requestMap)
     return
 
   let resolve = requestMap.resolve
     , reject = requestMap.reject
+    , response = message.Response
 
-  delete this._requestMap[msg.ActionID]
+  delete this._requestMap[message.ActionID]
+  delete message.Response
 
-  if (msg.Response == 'Error') {
-    reject(new Error(msg.Message))
+  if (response == C.ACTION_RESPONSE_ERROR) {
+    reject(new Error(message.Message))
   } else {
-    resolve(msg)
+    resolve(message)
   }
 }
 
